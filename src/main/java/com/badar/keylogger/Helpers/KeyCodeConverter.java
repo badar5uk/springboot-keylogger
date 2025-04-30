2755fb5d01a928b7ae5421acdc46e81518a75d30
Badar Al-Busaidi <bader5uk@hotmail.com>
2025-04-30 10:38:16 +0400

    Added helper util to convert code to key

---
+++ src/main/java/com/badar/keylogger/Helpers/KeyCodeConverter.java
@@ -0,0 +1,11 @@
+package com.badar.keylogger.Helpers;
+
+public class KeyCodeConverter {
+
+    // store the state in a byte
+    private static final byte[] keyboardState = new byte[256];
+
+
+    public static void vkCodeToKey(int vkCode, boolean shiftPressed) {
+    }
+}
